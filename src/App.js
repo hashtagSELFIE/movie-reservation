5eb479078b494125b625346efcc89e7a23a4be21
chastiefol <kunza70@hotmail.com>
2021-04-20 00:01:07 +0700

    Update Router

--- src/App.js
+++ src/App.js
@@ -4,17 +4,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const Home = () => <h1>Home</h1>
-const About = () => <h1>About</h1>
-const Post = () => <h1>Post</h1>
-const Project = () => <h1>Project</h1>
+const Movies = () => <h1>Movies</h1>
+const Login = () => <h1>Login</h1>
 
 function App() {
   return (
+    <div className="my-app">
+ <nav className="navbar navbar-expand-lg navbar-light bg-light px-5">
+  <a className="navbar-brand" href="#">Theater</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/movies">Movies</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/login">Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <div className="App container">
       <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/posts" component={Post} />
-      <Route exact path="/projects" component={Project} />
+      <Route path="/movies" component={Movies} />
+      <Route path="/login" component={Login} />
+    </div>
   </div>
   );
 }
