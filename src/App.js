73a6281d699f114bc4d747e2f262868d686c988a
chastiefol <kunza70@hotmail.com>
2021-04-20 21:03:52 +0700

    Add new components

--- src/App.js
+++ src/App.js
@@ -3,39 +3,40 @@ import { Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-const Home = () => <h1>Home</h1>
+import  Home  from "./views/Home.js"
+
 const Movies = () => <h1>Movies</h1>
 const Login = () => <h1>Login</h1>
 
 function App() {
   return (
     <div className="my-app">
- <nav className="navbar navbar-expand-lg navbar-light bg-light px-5">
-  <a className="navbar-brand" href="#">Theater</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light px-5">
+        <a className="navbar-brand" href="#">Theater</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="/">Home</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/movies">Movies</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/login">Login</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-    <div className="App container">
-      <Route exact path="/" component={Home} />
-      <Route path="/movies" component={Movies} />
-      <Route path="/login" component={Login} />
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/movies">Movies</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/login">Login</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/login" component={Login} />
+      </div>
     </div>
-  </div>
   );
 }
 
---
+++ src/components/Banner.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+const Banner = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} id="carousel" onSelect={handleSelect} className={"w-100 p-1"}>
+      <Carousel.Item>
+        <img
+          className="d-flex w-100"
+          src="https://picsum.photos/id/237/1024/300"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-flex w-100"
+          src="https://picsum.photos/id/238/1024/300"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-flex w-100"
+          src="https://picsum.photos/id/239/1024/300"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Banner;
